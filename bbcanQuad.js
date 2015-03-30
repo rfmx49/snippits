b2ee5204806dd007690baad0e982d91dc4f07e2d
rfmx49 <rfmx49@gmail.com>
2015-03-30 15:45:15 -0400

    Different table views

--- bbcanQuad.js
+++ bbcanQuad.js
@@ -6,7 +6,6 @@
 // @author        rfmx49
 // @version       .2
 // ==/UserScript==
-
 $( document ).ready(function() {
     $(".feed__headline").after('<div id="quadButton">Quad View</div>');
 
@@ -16,7 +15,6 @@ $( document ).ready(function() {
 });
 
 function quadView() {
-        
         //Do not change unless feed links have changed.
 		var feeds = ["3744372","3744338","3744354","3744360","3744365"];
 		//Fan feed 0
@@ -49,9 +47,10 @@ function quadView() {
         $(".player").remove();
         $(".footer__copyright").remove();
         
-        //create quad table
-        $(".feed-column").html('<table><tr><td id="feed1">1</td><td id="feed2">2</td></tr><tr><td id="feed3">3</td><td id="feed4">4</td></tr><tr><td id="feed5" colspan=2>5</td></tr></table>')
-  
+        //This table has 4 feeds with fan feed showing below.
+        //$(".feed-column").html('<table><tr><td id="feed1">1</td><td id="feed2">2</td></tr><tr><td id="feed3">3</td><td id="feed4">4</td></tr><tr><td id="feed5" colspan=2>5</td></tr></table>');
+  		//This table has 3 feeds first row(fan/1/2) and feeds 3/4 on bottom.
+  		$(".feed-column").html('<table><tr><td colspan="2" id="feed5"></td><td colspan="2" id="feed1"></td><td colspan="2" id="feed2"></td></tr><tr><td colspan="6" height="25px"></td></td><tr><td colspan="3" id="feed3"></td><td colspan="3" id="feed4"></td></tr></table>');
  
         var feed1='<div class="live-feeds__iframe-container"><div ng-show="feedDisabled" class="live-feeds__disabled ng-hide"><img src="/assets/img/live-feeds/feed-disabled.jpg" class="live-feeds__disabled-image"></div><iframe id="live-feeds-iframe" ng-hide="feedDisabled" bbcan-iframe-resize="" ng-src="https://new.livestream.com/accounts/6388794/events/' + viewableFeeds[0] + '/player?pre_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_pre.jpg&amp;post_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_post.jpg&amp;width=' + feedWidth + '&amp;height=' + feedHeight + '&amp;autoPlay=' + autoPlay + '&amp;mute=' + muteStatus + '" frameborder="0" scrolling="no" class="live-feeds__iframe" src="https://new.livestream.com/accounts/6388794/events/' + viewableFeeds[0] + '/player?pre_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_pre.jpg&amp;post_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_post.jpg&amp;width=' + feedWidth + '&amp;height=' + feedHeight + '&amp;autoPlay=' + autoPlay + '&amp;mute=' + muteStatus + '" style="width: 100%; height: ' + controlsHeight + 'px;"></iframe></div>'
         var feed2='<div class="live-feeds__iframe-container"><div ng-show="feedDisabled" class="live-feeds__disabled ng-hide"><img src="/assets/img/live-feeds/feed-disabled.jpg" class="live-feeds__disabled-image"></div><iframe id="live-feeds-iframe" ng-hide="feedDisabled" bbcan-iframe-resize="" ng-src="https://new.livestream.com/accounts/6388794/events/' + viewableFeeds[1] + '/player?pre_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_pre.jpg&amp;post_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_post.jpg&amp;width=' + feedWidth + '&amp;height=' + feedHeight + '&amp;autoPlay=' + autoPlay + '&amp;mute=' + muteStatus + '" frameborder="0" scrolling="no" class="live-feeds__iframe" src="https://new.livestream.com/accounts/6388794/events/' + viewableFeeds[1] + '/player?pre_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_pre.jpg&amp;post_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_post.jpg&amp;width=' + feedWidth + '&amp;height=' + feedHeight + '&amp;autoPlay=' + autoPlay + '&amp;mute=' + muteStatus + '" style="width: 100%; height: ' + controlsHeight + 'px;"></iframe></div>'
