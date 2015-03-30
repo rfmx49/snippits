fc089ec098f2fd2a0499140a8bce009d9263d7df
rfmx49 <rfmx49@gmail.com>
2015-03-29 22:11:00 -0400

    Added fan feed and some configurable options

--- bbcanQuad.js
+++ bbcanQuad.js
@@ -16,8 +16,27 @@ $( document ).ready(function() {
 });
 
 function quadView() {
-        //page cleanup
-        $(".global-tv-header").remove();
+        
+        //Do not change unless feed links have changed.
+		var feeds = ["3744372","3744338","3744354","3744360","3744365"];
+		//Fan feed 0
+		//Feed 1-4 1-4
+		//SET YOUR FEEDS HERE
+		var viewableFeeds = [feeds[1],feeds[2],feeds[3],feeds[4]];
+		var bottomFeed = false;
+		var bottomFeed = feeds[0]; //comment out if you do not want to show bottom feed comment with two slasshes //
+		//SET OPTIONS
+		var autoPlay = "false";
+        var muteStatus = "true";
+        
+        //Change Feed dimensions
+        var feedHeight=360;
+        var feedWidth=600;
+        var controlsHeight = 380; //default 404
+		
+		
+		//page cleanup
+		$(".global-tv-header").remove();
         $(".site-header").remove();
         $(".feed__header").remove();
         $(".toolbar").remove();
@@ -29,23 +48,23 @@ function quadView() {
         $(".live-feeds__info").remove();
         $(".player").remove();
         $(".footer__copyright").remove();
+        
         //create quad table
-        $(".feed-column").html('<table><tr><td id="feed1">1</td><td id="feed2">2</td></tr><tr><td id="feed3">3</td><td id="feed4">4</td></tr></table>')
-        //set feed defaults
-        var feedHeight=360;
-        var feedWidth=600;
-        var controlsHeight = 380; //default 404
-        var autoPlay = "false";
-        var muteStatus = "true";
+        $(".feed-column").html('<table><tr><td id="feed1">1</td><td id="feed2">2</td></tr><tr><td id="feed3">3</td><td id="feed4">4</td></tr><tr><td id="feed5" colspan=2>5</td></tr></table>')
+  
  
-        var feed1='<div class="live-feeds__iframe-container"><div ng-show="feedDisabled" class="live-feeds__disabled ng-hide"><img src="/assets/img/live-feeds/feed-disabled.jpg" class="live-feeds__disabled-image"></div><iframe id="live-feeds-iframe" ng-hide="feedDisabled" bbcan-iframe-resize="" ng-src="https://new.livestream.com/accounts/6388794/events/3744338/player?pre_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_pre.jpg&amp;post_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_post.jpg&amp;width=' + feedWidth + '&amp;height=' + feedHeight + '&amp;autoPlay=' + autoPlay + '&amp;mute=' + muteStatus + '" frameborder="0" scrolling="no" class="live-feeds__iframe" src="https://new.livestream.com/accounts/6388794/events/3744338/player?pre_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_pre.jpg&amp;post_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_post.jpg&amp;width=' + feedWidth + '&amp;height=' + feedHeight + '&amp;autoPlay=' + autoPlay + '&amp;mute=' + muteStatus + '" style="width: 100%; height: ' + controlsHeight + 'px;"></iframe></div>'
-        var feed2='<div class="live-feeds__iframe-container"><div ng-show="feedDisabled" class="live-feeds__disabled ng-hide"><img src="/assets/img/live-feeds/feed-disabled.jpg" class="live-feeds__disabled-image"></div><iframe id="live-feeds-iframe" ng-hide="feedDisabled" bbcan-iframe-resize="" ng-src="https://new.livestream.com/accounts/6388794/events/3744354/player?pre_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_pre.jpg&amp;post_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_post.jpg&amp;width=' + feedWidth + '&amp;height=' + feedHeight + '&amp;autoPlay=' + autoPlay + '&amp;mute=' + muteStatus + '" frameborder="0" scrolling="no" class="live-feeds__iframe" src="https://new.livestream.com/accounts/6388794/events/3744354/player?pre_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_pre.jpg&amp;post_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_post.jpg&amp;width=' + feedWidth + '&amp;height=' + feedHeight + '&amp;autoPlay=' + autoPlay + '&amp;mute=' + muteStatus + '" style="width: 100%; height: ' + controlsHeight + 'px;"></iframe></div>'
-        var feed3='<div class="live-feeds__iframe-container"><div ng-show="feedDisabled" class="live-feeds__disabled ng-hide"><img src="/assets/img/live-feeds/feed-disabled.jpg" class="live-feeds__disabled-image"></div><iframe id="live-feeds-iframe" ng-hide="feedDisabled" bbcan-iframe-resize="" ng-src="https://new.livestream.com/accounts/6388794/events/3744360/player?pre_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_pre.jpg&amp;post_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_post.jpg&amp;width=' + feedWidth + '&amp;height=' + feedHeight + '&amp;autoPlay=' + autoPlay + '&amp;mute=' + muteStatus + '" frameborder="0" scrolling="no" class="live-feeds__iframe" src="https://new.livestream.com/accounts/6388794/events/3744360/player?pre_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_pre.jpg&amp;post_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_post.jpg&amp;width=' + feedWidth + '&amp;height=' + feedHeight + '&amp;autoPlay=' + autoPlay + '&amp;mute=' + muteStatus + '" style="width: 100%; height: ' + controlsHeight + 'px;"></iframe></div>'
-        var feed4='<div class="live-feeds__iframe-container"><div ng-show="feedDisabled" class="live-feeds__disabled ng-hide"><img src="/assets/img/live-feeds/feed-disabled.jpg" class="live-feeds__disabled-image"></div><iframe id="live-feeds-iframe" ng-hide="feedDisabled" bbcan-iframe-resize="" ng-src="https://new.livestream.com/accounts/6388794/events/3744365/player?pre_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_pre.jpg&amp;post_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_post.jpg&amp;width=' + feedWidth + '&amp;height=' + feedHeight + '&amp;autoPlay=' + autoPlay + '&amp;mute=' + muteStatus + '" frameborder="0" scrolling="no" class="live-feeds__iframe" src="https://new.livestream.com/accounts/6388794/events/3744365/player?pre_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_pre.jpg&amp;post_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_post.jpg&amp;width=' + feedWidth + '&amp;height=' + feedHeight + '&amp;autoPlay=' + autoPlay + '&amp;mute=' + muteStatus + '" style="width: 100%; height: ' + controlsHeight + 'px;"></iframe></div>'
- 
+        var feed1='<div class="live-feeds__iframe-container"><div ng-show="feedDisabled" class="live-feeds__disabled ng-hide"><img src="/assets/img/live-feeds/feed-disabled.jpg" class="live-feeds__disabled-image"></div><iframe id="live-feeds-iframe" ng-hide="feedDisabled" bbcan-iframe-resize="" ng-src="https://new.livestream.com/accounts/6388794/events/' + viewableFeeds[0] + '/player?pre_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_pre.jpg&amp;post_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_post.jpg&amp;width=' + feedWidth + '&amp;height=' + feedHeight + '&amp;autoPlay=' + autoPlay + '&amp;mute=' + muteStatus + '" frameborder="0" scrolling="no" class="live-feeds__iframe" src="https://new.livestream.com/accounts/6388794/events/' + viewableFeeds[0] + '/player?pre_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_pre.jpg&amp;post_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_post.jpg&amp;width=' + feedWidth + '&amp;height=' + feedHeight + '&amp;autoPlay=' + autoPlay + '&amp;mute=' + muteStatus + '" style="width: 100%; height: ' + controlsHeight + 'px;"></iframe></div>'
+        var feed2='<div class="live-feeds__iframe-container"><div ng-show="feedDisabled" class="live-feeds__disabled ng-hide"><img src="/assets/img/live-feeds/feed-disabled.jpg" class="live-feeds__disabled-image"></div><iframe id="live-feeds-iframe" ng-hide="feedDisabled" bbcan-iframe-resize="" ng-src="https://new.livestream.com/accounts/6388794/events/' + viewableFeeds[1] + '/player?pre_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_pre.jpg&amp;post_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_post.jpg&amp;width=' + feedWidth + '&amp;height=' + feedHeight + '&amp;autoPlay=' + autoPlay + '&amp;mute=' + muteStatus + '" frameborder="0" scrolling="no" class="live-feeds__iframe" src="https://new.livestream.com/accounts/6388794/events/' + viewableFeeds[1] + '/player?pre_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_pre.jpg&amp;post_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_post.jpg&amp;width=' + feedWidth + '&amp;height=' + feedHeight + '&amp;autoPlay=' + autoPlay + '&amp;mute=' + muteStatus + '" style="width: 100%; height: ' + controlsHeight + 'px;"></iframe></div>'
+        var feed3='<div class="live-feeds__iframe-container"><div ng-show="feedDisabled" class="live-feeds__disabled ng-hide"><img src="/assets/img/live-feeds/feed-disabled.jpg" class="live-feeds__disabled-image"></div><iframe id="live-feeds-iframe" ng-hide="feedDisabled" bbcan-iframe-resize="" ng-src="https://new.livestream.com/accounts/6388794/events/' + viewableFeeds[2] + '/player?pre_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_pre.jpg&amp;post_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_post.jpg&amp;width=' + feedWidth + '&amp;height=' + feedHeight + '&amp;autoPlay=' + autoPlay + '&amp;mute=' + muteStatus + '" frameborder="0" scrolling="no" class="live-feeds__iframe" src="https://new.livestream.com/accounts/6388794/events/' + viewableFeeds[2] + '/player?pre_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_pre.jpg&amp;post_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_post.jpg&amp;width=' + feedWidth + '&amp;height=' + feedHeight + '&amp;autoPlay=' + autoPlay + '&amp;mute=' + muteStatus + '" style="width: 100%; height: ' + controlsHeight + 'px;"></iframe></div>'
+        var feed4='<div class="live-feeds__iframe-container"><div ng-show="feedDisabled" class="live-feeds__disabled ng-hide"><img src="/assets/img/live-feeds/feed-disabled.jpg" class="live-feeds__disabled-image"></div><iframe id="live-feeds-iframe" ng-hide="feedDisabled" bbcan-iframe-resize="" ng-src="https://new.livestream.com/accounts/6388794/events/' + viewableFeeds[3] + '/player?pre_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_pre.jpg&amp;post_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_post.jpg&amp;width=' + feedWidth + '&amp;height=' + feedHeight + '&amp;autoPlay=' + autoPlay + '&amp;mute=' + muteStatus + '" frameborder="0" scrolling="no" class="live-feeds__iframe" src="https://new.livestream.com/accounts/6388794/events/' + viewableFeeds[3] + '/player?pre_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_pre.jpg&amp;post_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_post.jpg&amp;width=' + feedWidth + '&amp;height=' + feedHeight + '&amp;autoPlay=' + autoPlay + '&amp;mute=' + muteStatus + '" style="width: 100%; height: ' + controlsHeight + 'px;"></iframe></div>'
+ 		
  
         $('#feed1').html(feed1).width(feedWidth).height(controlsHeight-30);
         $('#feed2').html(feed2).width(feedWidth).height(controlsHeight-30);
         $('#feed3').html(feed3).width(feedWidth).height(controlsHeight+60);
         $('#feed4').html(feed4).width(feedWidth).height(controlsHeight+60);
+        if (bottomFeed) {
+        	var feed5='<div class="live-feeds__iframe-container"><div ng-show="feedDisabled" class="live-feeds__disabled ng-hide"><img src="/assets/img/live-feeds/feed-disabled.jpg" class="live-feeds__disabled-image"></div><iframe id="live-feeds-iframe" ng-hide="feedDisabled" bbcan-iframe-resize="" ng-src="https://new.livestream.com/accounts/6388794/events/' + bottomFeed + '/player?pre_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_pre.jpg&amp;post_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_post.jpg&amp;width=' + feedWidth + '&amp;height=' + feedHeight + '&amp;autoPlay=' + autoPlay + '&amp;mute=' + muteStatus + '" frameborder="0" scrolling="no" class="live-feeds__iframe" src="https://new.livestream.com/accounts/6388794/events/' + bottomFeed + '/player?pre_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_pre.jpg&amp;post_event_poster=https://cdn.livestream.com/events/bigbrothercanada/bbc_post.jpg&amp;width=' + feedWidth + '&amp;height=' + feedHeight + '&amp;autoPlay=' + autoPlay + '&amp;mute=' + muteStatus + '" style="width: 100%; height: ' + controlsHeight + 'px;"></iframe></div>'
+ 			$('#feed5').html(feed5).width(feedWidth).height(controlsHeight+60);
+        }
 }
